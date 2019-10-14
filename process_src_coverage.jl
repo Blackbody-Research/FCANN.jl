ab52fdda251d43f2c4c961841fbe2408ae94c58f
Jason Eckstein <jekyllstein@gmail.com>
2019-10-14 04:50:56 -0600

    Update process_src_coverage.jl

--- process_src_coverage.jl
+++ process_src_coverage.jl
@@ -1,20 +1,22 @@
- function process_folder(folder="src")
-        @info "Coverage.process_folder: Searching $folder for .jl files..."
-        source_files = FileCoverage[]
-        files = readdir(folder)
-        for file in files
-            fullfile = joinpath(folder, file)
-            if isfile(fullfile)
-                # Is it a Julia file?
-                if splitext(fullfile)[2] == ".jl"
-                    push!(source_files, process_file(fullfile, folder))
-                else
-                    @debug "Coverage.process_folder: Skipping $file, not a .jl file"
-                end
-            elseif isdir(fullfile)
-                # If it is a folder, recursively traverse
-                append!(source_files, process_folder(fullfile))
+using Coverage
+
+function process_folder(folder="src")
+    @info "Coverage.process_folder: Searching $folder for .jl files..."
+    source_files = FileCoverage[]
+    files = readdir(folder)
+    for file in files
+        fullfile = joinpath(folder, file)
+        if isfile(fullfile)
+            # Is it a Julia file?
+            if splitext(fullfile)[2] == ".jl"
+                push!(source_files, process_file(fullfile, folder))
+            else
+                @debug "Coverage.process_folder: Skipping $file, not a .jl file"
             end
+        elseif isdir(fullfile)
+            # If it is a folder, recursively traverse
+            append!(source_files, process_folder(fullfile))
         end
-        return source_files
     end
+    return source_files
+end
